f9c2950abb08ad1ef6abd14b702444b76b45c320
NikitaZarochentsev <nikita.zarochencev@gmail.com>
2020-08-18 19:39:01 +0300

    Add toast notifications

--- AndroidCourse/Kotlin/Lesson_1_1/app/src/main/java/com/example/lesson_1_1/MainActivity.kt
+++ AndroidCourse/Kotlin/Lesson_1_1/app/src/main/java/com/example/lesson_1_1/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.lesson_1_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -14,21 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        this.studentList = TreeSet<String>()
+        this.studentList = TreeSet()
 
-        saveInArrayButton.setOnClickListener(View.OnClickListener {
-            if (editText.text.toString().trim().length != 0) {
-                var name = editText.text.toString()
+        saveInArrayButton.setOnClickListener {
+            if (this.editText.text.toString().trim().isNotEmpty()) {
+                val name = editText.text.toString()
                 this.studentList.add(name)
-            }
-        })
 
-        outputButton.setOnClickListener(View.OnClickListener {
-            textView.setText("")
-            for (student in studentList) {
-                var newStudentLine = textView.text.toString() + student + "\n"
+                Toast.makeText(this, "Имя $name сохранено", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Заполните поле ввода", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        outputButton.setOnClickListener {
+            this.textView.text = ""
+            for (student in this.studentList) {
+                val newStudentLine = textView.text.toString() + student + "\n"
                 textView.setText(newStudentLine)
             }
-        })
+
+            Toast.makeText(
+                this,
+                "Выведено ${this.studentList.size.toString()} элементов",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
