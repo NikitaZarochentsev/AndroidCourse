db1f26352ba72606708f1a1b0cbb62c0c12ac316
NikitaZarochentsev <nikita.zarochencev@gmail.com>
2020-09-09 19:44:32 +0300

    Add activity without ConstrainLayout with basic toolbar

--- AndroidCourse/Java/Lesson_3/app/src/main/java/com/example/lesson_3/MainActivity.java
+++ AndroidCourse/Java/Lesson_3/app/src/main/java/com/example/lesson_3/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.lesson_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonWithoutCL = findViewById(R.id.buttonWithoutCL);
+        buttonWithoutCL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, WithoutCLActivity.class));
+            }
+        });
     }
 }
---
+++ AndroidCourse/Java/Lesson_3/app/src/main/java/com/example/lesson_3/WithoutCLActivity.java
@@ -0,0 +1,33 @@
+package com.example.lesson_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class WithoutCLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_without_cl);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
