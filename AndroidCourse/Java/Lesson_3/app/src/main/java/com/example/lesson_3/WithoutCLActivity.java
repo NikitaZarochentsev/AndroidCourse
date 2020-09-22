cb6ab7998a851313115088a3a3d60953736242e7
NikitaZarochentsev <nikita.zarochencev@gmail.com>
2020-09-22 21:38:40 +0300

    Add activity withour CL

--- AndroidCourse/Java/Lesson_3/app/src/main/java/com/example/lesson_3/WithoutCLActivity.java
+++ AndroidCourse/Java/Lesson_3/app/src/main/java/com/example/lesson_3/WithoutCLActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WithoutCLActivity extends AppCompatActivity {
 
@@ -15,12 +16,28 @@ public class WithoutCLActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_without_cl);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        TextView textViewName = findViewById(R.id.textViewName);
+        textViewName.setText("Анастасия");
+        TextView textViewSurname = findViewById(R.id.textViewSurname);
+        textViewSurname.setText("Антонина");
+        TextView textViewEmail = findViewById(R.id.textViewEmail);
+        textViewEmail.setText("anykee.box@gmail.ru");
+        TextView textViewLogin = findViewById(R.id.textViewLogin);
+        textViewLogin.setText("HIE023UOI88");
+        TextView textViewRegion = findViewById(R.id.textViewRegion);
+        textViewRegion.setText("Санкт-Петербург");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
