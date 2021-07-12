e3ebb6966fe507efaa487793e43f25c7f85b950e
Nikita Zarochentsev <nikita.zarochencev@gmail.com>
2021-07-13 02:38:40 +0300

    Add Activity 4

--- AndroidCourse/Java/Lesson_5/app/src/main/java/com/example/lesson_5/MainActivity.java
+++ AndroidCourse/Java/Lesson_5/app/src/main/java/com/example/lesson_5/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lesson_5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.buttonToAct4Main);
+        button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Activity4.class)));
     }
 }
